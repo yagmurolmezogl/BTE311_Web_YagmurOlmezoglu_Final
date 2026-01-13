eb79bd0285777cfc3df92d5560c2255daad5e1f3
yagmurolmezoglu <yolmezoglu7@gmail.com>
2026-01-13 14:47:17 +0300

    2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="text-white p-4 text-center mb-4" style={{backgroundColor: 'rgba(0,0,0,0.5)'}}>
       <h1 className="display-4">🌦️ Hava Durumu</h1>
       <p className="lead">React & Open-Meteo API Final Ödevi</p>
-      <a href="https://github.com/yourusername/hava-durumu-app" className="btn btn-outline-light">GitHub Repo</a>
+      <a href="https://github.com/yagmurolmezogl/BTE311_Web_YagmurOlmezoglu_Final" className="btn btn-outline-light">GitHub Repo</a>
     </header>
   );
 };
